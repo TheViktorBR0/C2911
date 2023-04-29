9aaa1f24d56bd44485c0a132c51880a3eba362d1
TheViktorBR0 <102690696+TheViktorBR0@users.noreply.github.com>
2023-04-29 09:59:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-class Work:
-    print("Oh, it's working!")
-ItShouldWork = Work
+class Book:
+    print("I am book -_-")
+    def __init__(self):
+        self.color = 'red'
+        print("And i am red -_-")
+
+first_book = Book
+Book.__init__(self=first_book)
