9a7783ffd060307f8f57b68979e7379967ab3cd1
TheViktorBR0 <102690696+TheViktorBR0@users.noreply.github.com>
2023-05-06 09:18:11 +0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+class Human:
+    def __init__(self, name='Human', job=None, home=None, car=None):
+        self.name = name
+        self.money = 100
+        self.gladness = 50
+        self.satiety = 50
+        self.job = job
+        self.car = car
+        self.home = home
