e6925bdfa9e07f539bf1993aa07cc16ef9859a5d
TheViktorBR0 <102690696+TheViktorBR0@users.noreply.github.com>
2023-05-06 08:46:23 +0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+class Human:
+    def __init__(self, name='Human'):
+        self.name = name
+
+class Auto:
+    def __init__(self, brand):
+        self.brand = brand
+        self.passengers = []
+
+    def add_passenger(self, human):
+        self.passengers.append(human)
+
+    def print_passengers_names(self):
+        if self.passengers != []:
+            
