868440999e02ba4720f634d5c470cd74dfa4e26a
TheViktorBR0 <102690696+TheViktorBR0@users.noreply.github.com>
2023-05-20 10:30:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -37,13 +37,13 @@
 #
 # ----------------------------------------------------------------------------------
 
-class Helper:
-    def __init__(self, work):
-        self.work = work
-    def __call__(self, work):
-        return f"I will help you with your {self.work}. Afterwards i will help you with {work}"
-
-helper = Helper('Homework')
-print(helper('Cleaning'))
+# class Helper:
+#     def __init__(self, work):
+#         self.work = work
+#     def __call__(self, work):
+#         return f"I will help you with your {self.work}. Afterward I will help you with {work}"
+#
+# helper = Helper('Homework')
+# print(helper('Cleaning'))
 
 # ----------------------------------------------------------------------------------
