1b1cd88f35d3fa7a75817f7136a32af416cf0fbe
TheViktorBR0 <102690696+TheViktorBR0@users.noreply.github.com>
2023-05-20 10:58:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -81,3 +81,25 @@
 # checker(calculate, '10+10')
 
 # ----------------------------------------------------------------------------------
+
+def checker(*exc_types):
+    def checker(function):
+        def checker(*args, **kwargs):
+            try:
+                result = function(*args, **kwargs)
+            except (exc_types) as exc:
+                print(f"We have a problems {exc}")
+            else:
+                print(f"No problems. Result - {result}")
+
+        return checker
+
+    return checker
+
+@checker(NameError, TypeError, SyntaxError, ZeroDivisionError)
+def calculate(expression):
+    return eval(expression)
+
+calculate(input())
+
+# ----------------------------------------------------------------------------------
