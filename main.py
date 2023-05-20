d52d32acf8e67e3652bd9c5c030c5f92c90bd093
TheViktorBR0 <102690696+TheViktorBR0@users.noreply.github.com>
2023-05-20 09:12:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# --------------------------------------------------------------------------------
+#
 # print(f"NameError - {type(NameError)} - {issubclass(NameError, BaseException)}")
 #
 # --------------------------------------------------------------------------------
@@ -28,14 +30,47 @@
 # print('next code')
 #
 # --------------------------------------------------------------------------------
-
-def checker(a):
-    if type(a) != str:
-        raise TypeError(f"Sorry, we can't work with {type(a),}, we need class str")
-    else:
-        return a
-
-var = 10
-checker(var)
-
+#
+#
+# def checker(a):
+#     if type(a) != str:
+#         raise TypeError(f"Sorry, we can't work with {type(a),}, we need class str")
+#     else:
+#         return a
+#
+#
+# var = 10
+# checker(var)
+#
+# --------------------------------------------------------------------------------
+#
+#
+# class BuildingError(Exception):
+#     def __str__(self):
+#         return f"With so much material the house can't built"
+#
+#
+# def check_material(amount_of_material, limit_value):
+#     if amount_of_material > limit_value:
+#         return 'enough materials'
+#     else:
+#         raise BuildingError(amount_of_material)
+#
+#
+# materials = 300.0000000000001
+# print(check_material(materials, 300))
+#
+# --------------------------------------------------------------------------------
+#
+# import warnings
+#
+# warnings.simplefilter('ignore', SyntaxWarning)
+# warnings.simplefilter('error', ImportWarning)
+#
+# warnings.warn('Warning, no code here', SyntaxWarning)
+# try:
+#     warnings.warn('Warning, module not imported', ImportWarning)
+# except:
+#     print('warning processed')
+#
 # --------------------------------------------------------------------------------
