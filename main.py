ec7a42eaf3258eac84d4c8ec38e957db33733c29
TheViktorBR0 <102690696+TheViktorBR0@users.noreply.github.com>
2023-05-25 17:51:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 result = []
 
 
+def problems(data):
+    try:
+        result = eval(data)
+        return result
+    except SyntaxError as error:
+        return f"Syntax, Error {error}"
+    except NameError as error:
+        return f"Name Error, {error}"
+    except TypeError as error:
+        return f"Type Error, {error}"
+    except ZeroDivisionError as error:
+        return f"Error, {error}"
+
+
 def divider(a, b):
     if a < b:
         raise ValueError
     if b > 100:
         raise IndexError
     return a / b
-
-    try:
-        result = eval(data)
-        return result
-    except:
-        SyntaxError as error:
-        return f"Syntax, Error {error}"
 
 
 data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8: 4}
