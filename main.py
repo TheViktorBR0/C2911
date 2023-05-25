fdc5acfdb31d97158cc970c1af9465fc61257e71
TheViktorBR0 <102690696+TheViktorBR0@users.noreply.github.com>
2023-05-25 17:47:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ def divider(a, b):
         raise IndexError
     return a / b
 
-try:
+    try:
     SyntaxError as error:
-    return f"Syntax, Error {error}"
+        return f"Syntax, Error {error}"
+    except:
+
 
 
 data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8: 4}
