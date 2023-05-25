2bb9a38627b8d4c13dff2489debb61dbd445d8ae
TheViktorBR0 <102690696+TheViktorBR0@users.noreply.github.com>
2023-05-25 17:08:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,29 +6,9 @@ def divider(a, b):
     if b > 100:
         raise IndexError
     return a/b
-
 data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
-
 for key in data:
     res = divider(key, data[key])
     result.append(res)
-    try:
-        result = eval()
-        return result
-    except SyntaxError as error:
-        return f"Syntax, Error {error}"
 
 print(result)
-
-def problems(data):
-    try:
-        result = eval(data)
-        return result
-    except SyntaxError as error:
-        return f"Syntax, Error {error}"
-    except NameError as error:
-        return f"Name Error, {error}"
-    except TypeError as error:
-        return f"Type Error, {error}"
-    except ZeroDivisionError as error:
-        return f"Error, {error}"
