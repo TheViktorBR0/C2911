49308671e07ee7470bac6ad225d9f2132d68c9bc
TheViktorBR0 <102690696+TheViktorBR0@users.noreply.github.com>
2023-05-26 22:14:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,35 @@ result = []
 
 
 def divider(a, b):
-    if a < b:
+    if type(a) != int:
+        raise TypeError
+
+    elif a < b:
         raise ValueError
 
-    elif a or b != int:
-        raise TypeError
+    elif b > 100:
+        raise IndexError
 
     elif b == 0:
         raise ZeroDivisionError
 
-    elif b > 100:
-        raise IndexError
     return a / b
 
 
-data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8: 4}
+data = {10: 2, 1: 5, "123": 4, 18: 0, []: 15, 8: 4}
 
 for key in data:
     res = divider(key, data[key])
     result.append(res)
 
 print(result)
+
+# def checker(a):
+#     if type(a) != str:
+#         raise TypeError(f"Sorry, we can't work with {type(a),}, we need class str")
+#     else:
+#         return a
+#
+#
+# var = 10
+# checker(var)
