611c2bfefecec58af75186d914eca0961b30747f
TheViktorBR0 <102690696+TheViktorBR0@users.noreply.github.com>
2023-06-03 09:06:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ image = cv2.imread(image_path)
 cat_face = cat_face_cascade.detectMultiScale(image)
 print(cat_face)
 
-cv2.imshow('Cat', image)
+for (x, y, w, h) in cat_face:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 3)
+
+cv2.imshow("Cat", image)
 cv2.waitKey()
