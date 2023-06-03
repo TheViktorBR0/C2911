f56e3a33c10ecb385ab103eecc6011dabff2cbe1
TheViktorBR0 <102690696+TheViktorBR0@users.noreply.github.com>
2023-06-03 08:56:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import cv2
 
 image_path = 'cat.jpg'
-
-cat_face_cascade = cv2.CascadeClassifire('haarcascade_frontalcatface_extended.xml')
-
+cat_face_cascade = cv2.CascadeClassifier('haarcascade_frontalcatface_extended.xml')
 image = cv2.imread(image_path)
-
+cat_face = cat_face_cascade.detectMultiScale(image)
+print(cat_face)
 cv2.imshow('Cat', image)
 cv2.waitKey()
