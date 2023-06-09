e3c8845831e85ce4fd637c9b7942e83ebc48eba0
TheViktorBR0 <102690696+TheViktorBR0@users.noreply.github.com>
2023-06-09 21:55:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ for parse_elem1 in responce_parse:
     for parse_elem2 in parse_elem1.split("</span>"):
         if parse_elem2[1].isdigit():
             cours = float(price) / float(parse_elem2)
+            print("Rounded price in dollars($) is: ")
             print(round(cours, 2), '$')
-
-
+            quit()
