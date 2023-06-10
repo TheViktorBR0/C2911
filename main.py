96d19a6397915ef8b04225585a2934a92429c4ad
TheViktorBR0 <102690696+TheViktorBR0@users.noreply.github.com>
2023-06-10 08:34:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import random
+import cv2
+from PIL import Image
 
 print('Welcome to the library of all our projects!')
 print('What project you want to see?')
@@ -398,7 +400,43 @@ def calculator():
 # ---------------------------------------------------------------------------------------------------------------
 
 def ai():
-    
+    image_path = 'cat.jpg'
+    cat_face_cascade = cv2.CascadeClassifier('haarcascade_frontalcatface_extended.xml')
+    image = cv2.imread(image_path)
+
+    cat_face = cat_face_cascade.detectMultiScale(image)
+
+    cat = Image.open(image_path)
+    glasses = Image.open('glasses.png')
+    cat = cat.convert('RGBA')
+
+    for (x, y, w, h) in cat_face:
+        glasses = glasses.resize((w, int(h / 3)))
+        cat.paste(glasses, (x, int(y + h / 4)), glasses)
+        cat.save('cat_with_glasses.png')
+        cat_with_glasses = cv2.imread('cat_with_glasses.png')
+        cv2.imshow('cat_with_glasses.png', cat_with_glasses)
+        cv2.waitKey()
+
+    # ------------------------------------------------------------------
+
+    image_path = 'dog.jpg'
+    dog_face_cascade = cv2.CascadeClassifier('dog_face.xml')
+    image = cv2.imread(image_path)
+
+    dog_face = dog_face_cascade.detectMultiScale(image)
+
+    dog = Image.open(image_path)
+    bubble = Image.open('bubble.png')
+    dog = dog.convert('RGBA')
+
+    for (x, y, w, h) in dog_face:
+        bubble = bubble.resize((w, int(h / 1.4)))
+        dog.paste(bubble, (x, int(y + h / 1.5)), bubble)
+        dog.save('dog_with_bubble.png')
+        dog_with_glasses = cv2.imread('dog_with_bubble.png')
+        cv2.imshow('dog_with_bubble.png', dog_with_glasses)
+        cv2.waitKey()
 
 # ---------------------------------------------------------------------------------------------------------------
 
