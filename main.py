4b2a706052838a7a100fb593f9ad649819dec7a6
TheViktorBR0 <102690696+TheViktorBR0@users.noreply.github.com>
2023-06-10 08:12:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 print('Welcome to the library of all our projects!')
 print('What project you want to see?')
-input('Variants: Students life, Cats life, Bank account, Sims, ')
+print('Variants: Students life, Cats life, Bank account, Sims, Calculator, AI or Currency converter: ')
+choise = input('').lower()
+
+if choise == 'Students life'.lower():
+    
