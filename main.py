0233d526d7f1999a4e3c94a04ab18149c6cd6a91
TheViktorBR0 <102690696+TheViktorBR0@users.noreply.github.com>
2023-11-04 08:37:46 +0100

    Update main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+#finding the perimeter of a rectangle
+length = 5
+width = 2
+print(f'{length*2+width*2}cm - perimeter')
+
+#dinding the total cost of 3 full complected computers
+cost_mouse = 12
+cost_keyboard = 15
+cost_computer = 50
+
+total_cost = cost_computer + cost_mouse + cost_keyboard
+print(f'{total_cost*3}$ - total cost of 3 full complected computers')
+
+celsius = int(input("Enter the temperature in celsius: "))
+fahrenheit = (1.8 * celsius) + 32.
+print("Temperature in fahrenheit :", fahrenheit)
