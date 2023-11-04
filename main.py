a4ecd31b9d873cd02217f344f1df561275caaf58
TheViktorBR0 <102690696+TheViktorBR0@users.noreply.github.com>
2023-11-04 08:58:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-#finding the perimeter of a rectangle
+# finding the perimeter of a rectangle
 length = 5
 width = 2
-print(f'{length*2+width*2}cm - perimeter')
+print(f'{length * 2 + width * 2}cm - perimeter')
 
-#finding the total cost of 3 full complected computers
+# finding the total cost of 3 full complected computers
 cost_mouse = 12
 cost_keyboard = 15
 cost_computer = 51
 
 total_cost = cost_computer + cost_mouse + cost_keyboard
-print(f'{total_cost*3}$ - total cost of 3 full complected computers')
+print(f'{total_cost * 3}$ - total cost of 3 full complected computers')
 
-#celsius to another temperature scale converter
+# celsius to another temperature scale converter
 celsius = int(input("Enter the temperature in celsius: "))
 fahrenheit = (1.8 * celsius) + 32.
 print("Temperature in fahrenheit :", fahrenheit)
