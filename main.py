e6b2b3ebc3080590a94cc4e02261acf0d1a58d9e
TheViktorBR0 <102690696+TheViktorBR0@users.noreply.github.com>
2023-11-25 08:45:58 +0100

    Commit

---
+++ main.py
@@ -0,0 +1,87 @@
+print('-----------------------------------------------------')
+
+# Prompt user to input age and provide a message based on the input
+age = int(input("Insert your age: "))
+if age < 18:
+    print("You are still too young")
+else:
+    print("You are welcome")
+
+print('-----------------------------------------------------')
+
+# Create a list, sort it, and print the largest element in the list
+list1 = [1, 5, 7, 1, 6, 9, 2, 7, 3]
+list1.sort()
+print("Largest element is:", list1[-1])
+
+print('-----------------------------------------------------')
+
+# Prompt user to input a number and display its multiplication table up to 10
+number = int(input("Input a number: "))
+
+multiply1 = number * 1
+multiply2 = number * 2
+multiply3 = number * 3
+multiply4 = number * 4
+multiply5 = number * 5
+multiply6 = number * 6
+multiply7 = number * 7
+multiply8 = number * 8
+multiply9 = number * 9
+multiply10 = number * 10
+
+print(f"{number} * 1 = {multiply1}")
+print(f"{number} * 2 = {multiply2}")
+print(f"{number} * 3 = {multiply3}")
+print(f"{number} * 4 = {multiply4}")
+print(f"{number} * 5 = {multiply5}")
+print(f"{number} * 6 = {multiply6}")
+print(f"{number} * 7 = {multiply7}")
+print(f"{number} * 8 = {multiply8}")
+print(f"{number} * 9 = {multiply9}")
+print(f"{number} * 10 = {multiply10}")
+
+print('-----------------------------------------------------')
+
+# Prompt user to input a sentence and display its reversed form
+sentence = input("Write any sentence: ")
+reversed = sentence[::-1]
+print("Reversed sentence: ", reversed)
+
+print('-----------------------------------------------------')
+
+# Define a factorial function and calculate the factorial of a provided number
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+num = int(input("Enter a number to calculate its factorial: "))
+
+if num < 0:
+    print("Factorial is not defined for negative numbers.")
+else:
+    fact = factorial(num)
+    print("The factorial of", num, "is", fact)
+
+print('-----------------------------------------------------')
+
+# Prompt user to input a number and determine if it's even or odd
+even_or_not = int(input("Enter a number: "))
+if even_or_not % 2 == 0:
+    print(even_or_not, "is an even number")
+else:
+    print(even_or_not, "is an odd number")
+
+print('-----------------------------------------------------')
+
+# Create lists, concatenate them, and display the individual lists and the concatenated list
+list2 = [1, 2]
+list3 = [3, 4]
+list4 = list2 + list3
+print(list2)
+print(list3)
+print(list4)
+
+print('-----------------------------------------------------')
